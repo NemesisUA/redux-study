7732894a432249e6ab7247e7d938b9361763f0fa
NemesisUA <display.n0ne.01.gmail.com>
2023-06-15 17:03:43 +0300

    base

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
 
 function App() {
+  const dispatch = useDispatch();
+  const cash = useSelector(state => state.cash.cash);
+
+  const addCash = (value) => {
+    dispatch({
+      type: 'ADD_CASH',
+      payload: value
+    })
+  }
+
+  const getCash = (value) => {
+    dispatch({
+      type: 'GET_CASH',
+      payload: value
+    })
+  }
+
   return (
     <div className="App">
-      Redux
+      <div className='cash-value'>{cash}</div>
+
+      <button onClick={() => addCash(Number(prompt('Add, $', '')))} >
+        Add Cash
+      </button>
+
+      <button onClick={() => getCash(Number(prompt('Get, $', '')))} >
+        Remove Cash
+      </button>
+      
     </div>
   );
 }
